e9730826ec6bc7258f137be6f794e3755feb78e0
xiaomeng li <xiaomeng li>
2017-10-23 21:27:54 +0800

    脚手架

---
+++ public/javascripts/simple_login.js
@@ -0,0 +1,9 @@
+Vue.component('simple-login',{
+    template:`<input id="email"></input><input id="pwd"></input>`,
+    data:function(){
+
+    },
+    methods:{
+
+    }
+})
---
+++ worker/mail_server.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+import smtplib
+import json
+from email.mime.text import MIMEText
+app = Flask(__name__)
+_user = "573912623@qq.com"
+_pwd  = "bpfoypnbzzqabdbd"
+_to   = "lixiaomeng19920528@outlook.com"
+def actSend(to,msg):
+	global _user,_pwd
+	msg=MIMEText(msg)
+	msg["Subject"]="确认邮件"
+	msg["From"]=_user
+	msg["To"]=_to
+	try:
+		s=smtplib.SMTP_SSL("smtp.qq.com",465)
+		s.login(_user,_pwd)
+		s.sendmail(_user,_to,msg.as_string())
+		s.quit()
+		print("Success!")
+	except e:
+		print("Failed,%s"%e)
+	pass
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+@app.route('/send_mail',methods=['POST'])
+def sendMail():
+	actSend(request.form["to"],request.form["msg"])
+	return json.dumps({"errno":0,"errmsg":"","data":""})
+if __name__ == '__main__':
+    app.run()
