16c37099e4f92b475073db178574b9a31e390743
xiaomeng li <xiaomeng li>
2017-10-25 00:09:04 +0800

    views

--- public/javascripts/simple_login.js
+++ public/javascripts/simple_login.js
@@ -1,9 +1,23 @@
-Vue.component('simple-login',{
-    template:`<input id="email"></input><input id="pwd"></input>`,
-    data:function(){
-
+Vue.component('simple-login', {
+    template: `<div><input v-bind:value="email"></input><input v-bind:value="pwd" type="password"></input></div>`,
+    data: function() {
+        return {
+            email: "",
+            pwd: ""
+        }
     },
-    methods:{
-
+    methods: {
+        setEmail: function(newemail) {
+            this.email = newemail.toString();
+        },
+        getEmail: function() {
+            return this.email;
+        },
+        setPasswd: function(newpwd) {
+            this.pwd = newpwd;
+        },
+        getPasswd: function() {
+            return this.pwd;
+        }
     }
 })
