8ef80cc613ca3919b37496d09d79cd8bbab59248
dalizitajiu <573912623@qq.com>
2017-10-24 13:57:46 +0800

    fix bug

--- launch.go
+++ launch.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
 	"crypto/md5"
 	"database/sql"
 	"encoding/base64"
@@ -19,7 +16,6 @@ import (
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
 	"github.com/mozillazg/request"
-	"menteslibres.net/gosexy/redis"
 )
 
 type Res struct {
@@ -40,16 +36,35 @@ type ArticleInfo struct {
 	Subtitle string `json:"subtitle"`
 	Content  string `json:"content"`
 }
+
 var db *sql.DB
 var red_client *redis.Client
-var stmt_insert *sql.Stmt
-var stmt_getbyauthor *sql.Stmt
+
+var register_key string = "awec12*"
+
 var new_article_key string = "article:"
+
+const mail_body string = `<html><body><a src="%s"></body></html>`
+const prefix_confirm string = "http://127.0.0.1:8080/register_confirm"
+const auth_key string = "auth123&"
+const article_key string = "article&#$12"
+
+var stmt_insert *sql.Stmt
+var stmt_update *sql.Stmt
+var stmt_getbyauthor *sql.Stmt
+var stmt_getarticle *sql.Stmt
+var stmt_userinfo *sql.Stmt
+
 var sql_insert string = "insert into article values(null,?,?,?,?)"
 var sql_getbyauthor string = "select * from article where author=?"
+var sql_updatearticle string = "update article set content=? where id=?"
+var sql_getarticle string = "select author,title,subtitle,content from article where id=?"
+
 func GetMd5(raw string) string {
 	return fmt.Sprintf("%x", md5.Sum([]byte(raw)))
 }
+
+//cookie参数序列化
 func Serialize(name string, value string, opt map[string]string) string {
 	result := make([]string, 0)
 	result = append(result, name+"="+value)
@@ -79,9 +94,12 @@ func NewRes(errno int, errmsg string, data interface{}) Res {
 func NewUserInfo(rid int, email string, nickname string, phone string, username string) UserInfo {
 	return UserInfo{rid, email, nickname, phone, username}
 }
+
 func NewAritcleInfo(author string, title string, subtitle string, content string) ArticleInfo {
 	return ArticleInfo{author, title, subtitle, content}
 }
+
+//发送邮件
 func SendEmail(to string, msg string) error {
 	c := new(http.Client)
 	req := request.NewRequest(c)
@@ -90,13 +108,19 @@ func SendEmail(to string, msg string) error {
 	defer resp.Body.Close()
 	return err
 }
+
+//字符串的base64加密
 func GetBase64(raw string) string {
 	return base64.StdEncoding.EncodeToString([]byte(raw))
 }
+
+//base64字符串的解码
 func DecodeBase64(raw string) (string, error) {
 	data, err := base64.StdEncoding.DecodeString(raw)
 	return string(data), err
 }
+
+//生成确认邮件的参数
 func GetConfirmUrl(email string, password string) string {
 	t := time.Now()
 	timestamp := strconv.Itoa(int(t.Unix()))
@@ -113,40 +137,27 @@ func GetConfirmUrl(email string, password string) string {
 	return prefix_confirm + "?" + strings.Join(reslist, "&")
 }
 
-const register_key = "awec12*"
-const mail_body string = `<html><body><a src="%s"></body></html>`
-const prefix_confirm string = "http://127.0.0.1:8080/register_confirm"
-const auth_key string = "auth123&"
-const article_key string = "article&#$12"
-
-var stmt_insert *sql.Stmt
-var stmt_update *sql.Stmt
-var stmt_getbyauthor *sql.Stmt
-var stmt_getarticle *sql.Stmt
-var new_article_key string = "article:"
-var sql_insert string = "insert into article values(null,?,?,?,?)"
-var sql_getbyauthor string = "select * from article where author=?"
-var sql_updatearticle string = "update article set content=? where id=?"
-var sql_getarticle string = "select author,title,subtitle,content from article where id=?"
->>>>>>> origin/master
-
 func GetTags(raw string) []string {
 	relist := make([]string, 0)
 	return append(relist, "sfsf")
 }
+
+//获取当前的时间戳
 func GetNow() string {
 	return strconv.Itoa(int(time.Now().Unix()))
 }
 
-//ruc校验
+//ruc token校验
 func Auth1(ri string, rtime string, rtoken string) bool {
 	return GetMd5(auth_key+ri+rtime) == rtoken
 }
 
-//蚊帐更新校验
+//文章更新token校验
 func Auth2(ri string, articleid int, aricletoken string) bool {
 	return GetMd5(article_key+ri+strconv.Itoa(articleid)) == aricletoken
 }
+
+//增加新文章
 func AddNewArticle(author string, title string, subtitle string, content string) error {
 	res, err := stmt_insert.Exec(author, title, subtitle, content)
 	if err != nil {
@@ -167,36 +178,16 @@ func AddNewArticle(author string, title string, subtitle string, content string)
 	_, err = red_client.HMSet(new_article_key+strconv.Itoa(int(id)), "author", author, "tags", raw, "createdtime", GetNow())
 	return err
 }
-
-func main() {
-	red_client = redis.New()
-	red_client.Connect("127.0.0.1", 6379)
-	//	red_client.Auth()
-	defer red_client.Quit()
-
-	db, err := sql.Open("mysql", "dev:dalizi1992@tcp(127.0.0.1:3306)/jack")
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	db.SetMaxOpenConns(20)
-	stmt_insert, _ = db.Prepare(sql_insert)
-	stmt_getbyauthor, _ = db.Prepare(sql_getbyauthor)
-
-	AddNewArticle("jack", "newtitle6", "subtitle2", "dfssfsfsfdf")
-	//	fmt.Println(id)
 func UpdateArticle(articleid int, content string) error {
-	res, err := stmt_update.Exec(content, articleid)
+	_, err := stmt_update.Exec(content, articleid)
 	if err != nil {
 		log.Println("mysql 错误")
 	}
 	return err
 }
 func GetArticleContent(articleid int) (string, string, string, string, error) {
-	res, err := stmt_getarticle.QueryRow(articleid)
-	if err != nil {
-		return "", "", "", "", err
-	}
+	res := stmt_getarticle.QueryRow(articleid)
+
 	var author string
 	var title string
 	var subtitle string
@@ -205,6 +196,7 @@ func GetArticleContent(articleid int) (string, string, string, string, error) {
 
 	return author, title, subtitle, content, nil
 }
+
 func main() {
 	app := iris.New()
 	//连接mysql数据库
@@ -293,10 +285,10 @@ func main() {
 		}
 
 		go SendEmail(mail, GetConfirmUrl(mail, passwd))
-		if err != nil {
-			log.Println("send mail fail")
-			log.Println(err)
-		}
+		//		if err != nil {
+		//			log.Println("send mail fail")
+		//			log.Println(err)
+		//		}
 		ctx.JSON(NewRes(0, "邮件已经发送", ""))
 	})
 	app.Get("/register_confirm", func(ctx context.Context) {
@@ -340,7 +332,7 @@ func main() {
 			ctx.JSON(NewRes(1005, "此链接已失效", ""))
 			return
 		}
-		rid, err1 := red_client.Get("regist:nextrid")
+		rid, err1 := red_client.Incr("regist:nextrid")
 		if err1 != nil {
 			log.Println(err1.Error())
 			ctx.JSON(NewRes(1004, "内部错误", ""))
@@ -358,7 +350,7 @@ func main() {
 	app.Post("/article/addnew", func(ctx context.Context) {
 		rtime := ctx.PostValue("r_time")
 		rtoken := ctx.PostValue("r_token")
-		ri := ctx.GetCookie("I")
+		rid := ctx.GetCookie("I")
 		author := ctx.PostValue("r_author")
 		title := ctx.PostValue("r_title")
 		subtitle := ctx.PostValue("r_subtitle")
@@ -367,11 +359,13 @@ func main() {
 			ctx.JSON(NewRes(1001, "参数错误", ""))
 			return
 		}
-		if (strconv.Atoi(GetNow()) - strconv.Atoi(rtime)) > 86400 {
+		t1, _ := strconv.Atoi(GetNow())
+		t2, _ := strconv.Atoi(rtime)
+		if (t1 - t2) > 86400 {
 			ctx.JSON(NewRes(800, "token过期", ""))
 			return
 		}
-		if !Auth1(ri, rtime, rtoken) {
+		if !Auth1(rid, rtime, rtoken) {
 			ctx.JSON(NewRes(1002, "token错误", ""))
 			return
 		}
@@ -384,11 +378,13 @@ func main() {
 	})
 	app.Post("/article/{id:int}", func(ctx context.Context) {
 		articleid, err := ctx.Params().GetInt("id")
-		rtime := ctx.FormValue("r_time")
-		rtoken := ctx.FormValue("r_token")
-		rid := ctx.GetCookie("I")
+		//		_ := ctx.FormValue("r_time")
+		//		_ := ctx.FormValue("r_token")
+		//		_ := ctx.GetCookie("I")
+		//		b1 := Auth1(rid, rtime, rtoken)
+
 		if err != nil || articleid <= 0 {
-			ctx.JSON(NewRes(1001, "不存在该页面"))
+			ctx.JSON(NewRes(1001, "不存在该页面", ""))
 			return
 		}
 		author, title, subtitle, content, err := GetArticleContent(articleid)
@@ -403,22 +399,23 @@ func main() {
 		rtime := ctx.PostValue("r_time")
 		rtoken := ctx.PostValue("r_token")
 		ri := ctx.GetCookie("I")
-		rarticleid, _ := ctx.PostValueInt("r_articleid")
+		rrarticleid := ctx.PostValue("r_articleid")
 		rarticletoken := ctx.PostValue("r_articletoken")
 		//		author := ctx.PostValue("r_author")
 		//		title := ctx.PostValue("r_title")
 		//		subtitle := ctx.PostValue("r_subtitle")
 		content := ctx.PostValue("r_content")
 
-		if ri == "" || rtime == "" || rtoken == "" || content == "" || raticleid == "" {
+		if ri == "" || rtime == "" || rtoken == "" || content == "" || rrarticleid == "" {
 			ctx.JSON(NewRes(1001, "参数错误", ""))
 			return
 		}
+		rarticleid, _ := strconv.Atoi(rrarticleid)
 		if !Auth1(ri, rtime, rtoken) {
 			ctx.JSON(NewRes(1002, "token错误", ""))
 			return
 		}
-		if !Auth2(ri, raticleid) {
+		if !Auth2(ri, rarticleid, rarticletoken) {
 			ctx.JSON(NewRes(1002, "token错误", ""))
 			return
 		}
